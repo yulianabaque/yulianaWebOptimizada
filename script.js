cdab8404cb1dfb57075d6887330bcde7bb0612c6
yuliana <gbaqued@esliceu.net>
2026-02-28 11:22:49 +0100

    js

--- script.js
+++ script.js
@@ -1,36 +1,27 @@
-const boton = document.getElementById("toggleCV");
-const detalles = document.getElementById("cvDetalles");
+const boton=document.getElementById("toggleCV");
+const detalles=document.getElementById("cvDetalles");
 
-boton.addEventListener("click", () => {
-  detalles.classList.toggle("oculto");
-
-  boton.textContent = detalles.classList.contains("oculto")
-    ? "Equip Loadout"
-    : "Unequip Loadout";
+boton.addEventListener("click",()=>{
+detalles.classList.toggle("oculto");
+boton.textContent=detalles.classList.contains("oculto")?"Equip Loadout":"Unequip Loadout"
 });
 
-const crosshair = document.getElementById("crosshair");
+const crosshair=document.getElementById("crosshair");
 
-document.addEventListener("mousemove", (e) => {
-  crosshair.style.left = e.clientX + "px";
-  crosshair.style.top = e.clientY + "px";
+document.addEventListener("mousemove",e=>{
+crosshair.style.left=e.clientX+"px";
+crosshair.style.top=e.clientY+"px"
 });
-const muzzle = document.getElementById("muzzle-flash");
 
-document.addEventListener("click", (e) => {
-  muzzle.style.left = e.clientX + "px";
-  muzzle.style.top = e.clientY + "px";
+const muzzle=document.getElementById("muzzle-flash");
 
-  muzzle.style.opacity = "1";
-  muzzle.style.transform =
-    "translate(-50%, -50%) scale(1.4) rotate(" +
-    (Math.random() * 30 - 15) +
-    "deg)";
-
-  setTimeout(() => {
-    muzzle.style.opacity = "0";
-    muzzle.style.transform = "translate(-50%, -50%) scale(0)";
-  }, 70);
-
-  
+document.addEventListener("click",e=>{
+muzzle.style.left=e.clientX+"px";
+muzzle.style.top=e.clientY+"px";
+muzzle.style.opacity="1";
+muzzle.style.transform="translate(-50%, -50%) scale(1.4) rotate("+(Math.random()*30-15)+"deg)";
+setTimeout(()=>{
+muzzle.style.opacity="0";
+muzzle.style.transform="translate(-50%, -50%) scale(0)"
+},70)
 });
