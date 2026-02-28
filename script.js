ebb2b6af9d1eb7e23043d548e8f007e2fa490bc9
yuliana <gbaqued@esliceu.net>
2026-02-28 11:38:22 +0100

    js

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
-const boton=document.getElementById("toggleCV");
-const detalles=document.getElementById("cvDetalles");
+const boton = document.getElementById("toggleCV");
+const detalles = document.getElementById("cvDetalles");
 
-boton.addEventListener("click",()=>{
-detalles.classList.toggle("oculto");
-boton.textContent=detalles.classList.contains("oculto")?"Equip Loadout":"Unequip Loadout"
-});
-
-const crosshair=document.getElementById("crosshair");
-
-document.addEventListener("mousemove",e=>{
-crosshair.style.left=e.clientX+"px";
-crosshair.style.top=e.clientY+"px"
-});
-
-const muzzle=document.getElementById("muzzle-flash");
-
-document.addEventListener("click",e=>{
-muzzle.style.left=e.clientX+"px";
-muzzle.style.top=e.clientY+"px";
-muzzle.style.opacity="1";
-muzzle.style.transform="translate(-50%, -50%) scale(1.4) rotate("+(Math.random()*30-15)+"deg)";
-setTimeout(()=>{
-muzzle.style.opacity="0";
-muzzle.style.transform="translate(-50%, -50%) scale(0)"
-},70)
+boton.addEventListener("click", () => {
+  detalles.classList.toggle("oculto");
+  boton.textContent = detalles.classList.contains("oculto")
+    ? "Equip Loadout"
+    : "Unequip Loadout";
 });
